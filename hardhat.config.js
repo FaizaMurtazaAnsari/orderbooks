60364c66a0f22fc69fea130c95d804411e880e91
Faiza Murtaza Ansari <58367920+FaizaMurtazaAnsari@users.noreply.github.com>
2023-05-19 00:28:33 +0500

    Update hardhat.config.js
    testing with multi localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,14 @@ module.exports = {
       chainId: 1337,
     },
   },
+//   networks: {
+//     polygon: {
+//       url: "https://polygon-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY", // Replace with your Alchemy API key or other Polygon provider URL
+//       chainId: 137,
+//       accounts:[0x93f99952f61434946858D8Cc1814Da9d91e4f9A9], // Add your private key here to sign transactions
+//     },
+//   },
+
   // paths: {
   //   artifacts: "./pages/artifacts",
   // }
